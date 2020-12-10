d84858ff035500e47bc23447243a9bce9e902007
Manuel Lopez Lopez <72388229+ManuelLopezLopez@users.noreply.github.com>
2020-12-10 13:04:40 +0100

    Update config.php

--- polideportivo/modelos/config.php
+++ polideportivo/modelos/config.php
@@ -1,6 +1,6 @@
 <?php
 
 define( 'DB_NAME', 'reservas-manuellopez' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', '');
+define( 'DB_USER', 'root' );//Manuel
+define( 'DB_PASSWORD', '');//Manuel_12345
 define( 'DB_HOST', 'localhost' );
